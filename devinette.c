ab5343ba1734310945954f9a472c42e97e219a39
Lyna Baouche <148858466+LynaBaouche@users.noreply.github.com>
2025-11-03 17:22:31 +0100

    Redemander si échec

--- devinette.c
+++ devinette.c
@@ -3,11 +3,13 @@
 int main() {
 	int nb_secret = 32;
 	int nb_user;
+	do{
 	scanf("%d", &nb_user);
 	if (nb_user == nb_secret) {
 		printf("Succès");
 	} else {
 		printf("Echec");
 	}
+} while (nb_user !=nb_secret);
 	return 0;
 }
